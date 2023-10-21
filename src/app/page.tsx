0989db3e6ab4cd1620a0e50b319bad5268de5688
David Sandoval <jsandovalbd1993@gmail.com>
2023-10-21 16:05:31 -0500

    build: :wrench: add support to aliases paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Container from '@ui/components/layout/Container/Container';
-
 export default function Home(): JSX.Element {
-  return (
-    <>
-      <Container>container</Container>
-    </>
-  );
+  return <>home</>;
 }
